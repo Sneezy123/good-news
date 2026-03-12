ac104fbf513c8574311031c015a45e4a368afaea
Sneezy123 <nilshendrik13@gmail.com>
2026-03-12 14:32:33 +0100

    change linking behaviour

--- app/components/ArticleItem.tsx
+++ app/components/ArticleItem.tsx
@@ -1,24 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
 import { ArticleType } from "../page";
-//import { decodeGoogleNewsUrl } from "@/server/decodeGoogleNewsURL";
+import Link from "next/link";
 
 export default function ArticleItem({ children }: { children: ArticleType }) {
-  //   const { data, isLoading, isSuccess } = useQuery<any>({
-  //     queryKey: ["urlDecode"],
-  //     queryFn: () => decodeGoogleNewsUrl(children.link),
-  //   });
   const titleArray = children.title.split(" - ");
   const publisher = titleArray[titleArray.length - 1];
   const title = titleArray.slice(0, titleArray.length - 1).join(" - ");
+  const sanitizedLink = children.link.match(/(?<=articles\/)\w+/gm);
+  const articleId = sanitizedLink ? sanitizedLink[0] : "";
+
   return (
-    <a
-      href={children.link}
+    <Link
+      href={`/${articleId}`}
       className="border-2 rounded-2xl border-outline border-solid m-3 p-5 overflow-clip transition-colors duration-100 hover:border-accent hover:animate-border-pulse flex"
     >
       <div className="">
         <p className="sm:text-base text-sm">{publisher}</p>
         <h1 className="sm:text-2xl text-lg text-foreground">{title}</h1>
       </div>
-    </a>
+    </Link>
   );
 }
